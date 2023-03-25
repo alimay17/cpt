5c2129959e31b7399c53ea1aea3c31d3a74c0505
Alice Smith <alimay.smith@outlook.com>
2023-03-24 21:06:16 -0600

    added pilot model and mock data

---
+++ src/app/pilots/mockPilotData.ts
@@ -0,0 +1,16 @@
+import { Pilot } from "./pilot.model";
+
+export const mockPilots: Pilot[] = [
+  {
+    id: 1000001,
+    AI: false,
+    name: 'Trea Mon Gishlalin',
+    status: 'Active'
+  },
+  {
+    id: 1000002,
+    AI: true,
+    name: 'C9-bx-279',
+    status: 'Inactive'
+  }
+]
---
+++ src/app/pilots/pilot.model.ts
@@ -0,0 +1,8 @@
+export class Pilot {
+  constructor(
+    public id: number,
+    public AI: boolean,
+    public name: string,
+    public status: string
+  ){}
+}
