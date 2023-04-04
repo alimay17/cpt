6a57ef9a193aafd9ee6f70337507454749f47bed
Alice Smith <alimay.smith@outlook.com>
2023-04-03 23:16:14 -0600

    implement delete ship

--- src/app/ships/ship.service.ts
+++ src/app/ships/ship.service.ts
@@ -11,14 +11,36 @@ export class ShipService {
   // properties
   ships: Ship[] = [];
 
-  //events
+  // events
   shipsChangedEvent = new Subject <Ship[]>();
 
   // constructor
   constructor() { }
 
-  getShips() {
+  /*============ Server Connection Methods =============*/
+  getShips(){
     this.ships = mockShips;
     this.shipsChangedEvent.next(this.ships.slice());
   }
+
+  getShip(id: string): Ship | null{
+    for (const ship of this.ships){
+      if(ship.shipId === id){
+        return ship;
+      };
+    };
+    return null;
+  }
+
+  // delete one ship
+  deleteShip(ship: Ship){
+    const pos = this.ships.findIndex(s => s.shipId === ship.shipId);
+    if (pos < 0) {
+      return;
+    }
+    this.ships.splice(pos, 1);
+    this.shipsChangedEvent.next(this.ships.slice());
+  }
+
+
 }
